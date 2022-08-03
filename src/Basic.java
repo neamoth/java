0421f765b3290a853b38c1636a0e241ce69a8266
Neamoth <neamothku@gmail.com>
2022-08-03 13:33:07 +0600

    feat: Do-while yes/no

--- src/Basic.java
+++ src/Basic.java
@@ -1,40 +1,43 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Basic {
     public static void main(String[] args) {
         /* This code is an according to the course formet of
         w3school
          */
-        System.out.print("Enter your name: ");
-        // UserInput
-        Scanner userinput = new Scanner(System.in);
-        String name = userinput.nextLine();
-        String userName = "Hello, ";
-        System.out.print("Enter your Age: ");
-        int age = userinput.nextInt();
-        System.out.println("Welcome to Bangladesh Road Transport Authority");
-        //String concatenation
-        System.out.println(userName + name );
-        System.out.println("Your age is, "+ age);
-        String status = "Driving Status:";
-        System.out.println(status);
-        //if-else
-        if(age>=18){
-            // Nasted if else
-            if (age<50) {
-                System.out.println("You are eligible for driving license");
-            }
-            else{
-                System.out.println("Sorry to say but, you can not drive in this age");
-            }
-        }else {
-            System.out.println("Sorry, You are underage to have a driving licence yet...!");
-        }
-        System.out.println("Enter a number To print serial");
-        int number = userinput.nextInt();
+
+        char choice;
+
+        // do-while loop
         do{
-            number = number+number;
-            System.out.println(number);
-        }while (number < 5);
+            // UserInput
+            Scanner userinput = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            String name = userinput.nextLine();
+            String userName = "Hello, ";
+            System.out.print("Enter your Age: ");
+            int age = userinput.nextInt();
+            System.out.println("Welcome to Bangladesh Road Transport Authority");
+            //String concatenation
+            System.out.println(userName + name );
+            System.out.println("Your age is, "+ age);
+            String status = "Driving Status:";
+            System.out.println(status);
+            //if-else
+            if(age>=18){
+                // Nasted if else
+                if (age<50) {
+                    System.out.println("You are eligible for driving license");
+                }
+                else{
+                    System.out.println("Sorry to say but, you can not drive in this age!");
+                }
+            }else {
+                System.out.println("Sorry, You are underage to have a driving licence yet...!");
+            }
+            System.out.println("Would you like to continue?(yes/no)");
+            choice = userinput.next().charAt(0);
+
+        }while (choice == 'y' || choice == 'Y');
     }
 }
