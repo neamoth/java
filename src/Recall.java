d21164abbb27bcb0717ebc3253b48356271c901b
Neamoth <neamothku@gmail.com>
2022-08-11 20:25:40 +0600

    Class object

---
+++ src/Recall.java
@@ -0,0 +1,8 @@
+public class Recall {
+    public void neamoth(){
+        System.out.println("Neamoth been recalled");
+    }
+    public void sakib(){
+        System.out.println("Sakib been recalled");
+    }
+}
